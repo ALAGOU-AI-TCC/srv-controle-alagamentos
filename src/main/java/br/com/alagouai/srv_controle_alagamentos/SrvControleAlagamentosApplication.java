b08c8d1e8fdb2838e5ddd105e07465d6f75b8bc8
Gabriel <briebr24@gmail.com>
2025-07-15 18:16:49 -0300

    Primeiro commit, estrutura do projeto

---
+++ src/main/java/br/com/alagouai/srv_controle_alagamentos/SrvControleAlagamentosApplication.java
@@ -0,0 +1,13 @@
+package br.com.alagouai.srv_controle_alagamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrvControleAlagamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrvControleAlagamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alagouai/srv_controle_alagamentos/SrvControleAlagamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alagouai.srv_controle_alagamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrvControleAlagamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
